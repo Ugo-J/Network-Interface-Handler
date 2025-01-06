f5732d49b289eab62ad66265fd0f650a6f3df041
Ugo-J <ezeorahJeffrey@gmail.com>
2025-01-06 13:35:44 +0100

    tweak for server test

--- net_interface_test.cpp
+++ net_interface_test.cpp
@@ -8,9 +8,9 @@ int main() {
     if(net_interface_handler::net_ns_unshare())
         return 1;
 
-    std::cout<<"Adding Interface "<<2<<" To Namespace\n";
-    handle.add_network_interface(2);
-    
+    std::cout<<"Adding Interface "<<1<<" To Namespace\n";
+    handle.add_network_interface(1);
+
     std::cout<<"Interface List After Adding To This Namespace\n";
     handle.get_network_interfaces();
 
