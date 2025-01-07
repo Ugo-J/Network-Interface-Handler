391132c3be2a53cb02aedb2e38336a2d2c78991f
Ugo-J <ezeorahJeffrey@gmail.com>
2025-01-07 19:45:21 +0100

    test change

--- net_interface_test.cpp
+++ net_interface_test.cpp
@@ -16,7 +16,8 @@ void thread_function(){
         net_handle.add_network_interface(1);
 
         handle2 = curl_easy_init();
-        curl_easy_setopt(handle2, CURLOPT_URL, "https://api.binance.com/api/v3/time");
+        curl_easy_setopt(handle2, CURLOPT_VERBOSE, 1L);
+        curl_easy_setopt(handle2, CURLOPT_URL, "https://34.160.111.145/");
         std::cout<<"IP Address From Thread 2\n";
         curl_easy_perform(handle2);
         std::cout<<std::endl;
@@ -39,7 +40,7 @@ int main() {
 
     while(!check);
 
-    curl_easy_setopt(handle, CURLOPT_URL, "https://api.binance.com/api/v3/time");
+    curl_easy_setopt(handle, CURLOPT_URL, "https://ifconfig.me/");
     std::cout<<"IP Address From Thread 1\n";
     curl_easy_perform(handle);
     //std::cout<<std::endl;
