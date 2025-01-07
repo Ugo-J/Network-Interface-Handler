abf2b8e80bbbf29c48a8343d55ed15ce5c6740a0
Ugo-J <ezeorahJeffrey@gmail.com>
2025-01-07 10:13:06 +0100

    test change

--- net_interface_test.cpp
+++ net_interface_test.cpp
@@ -1,8 +1,48 @@
 #include "network_interface_handler.hpp"
+#include <curl/curl.h>
+#include <thread>
+#include <atomic>
+
+CURL* handle;
+struct curl_slist * headers = NULL;
+CURL* handle2;
+net_interface_handler net_handle;
+std::atomic<bool> check = false;
+
+void thread_function(){
+    if(!net_interface_handler::net_ns_unshare()){
+
+        std::cout<<"Adding Interface "<<0<<" To Namespace\n";
+        net_handle.add_network_interface(0);
+
+        curl_easy_setopt(handle2, CURLOPT_URL, "https://ifconfig.me/");
+        std::cout<<"IP Address From Thread 2\n";
+        curl_easy_perform(handle2);
+        std::cout<<std::endl;
+        check = true;
+
+    }
+    
+}
 
 int main() {
-    net_interface_handler handle;
-    std::cout<<"Interface List Before Unshare\n";
+
+    curl_global_init(CURL_GLOBAL_ALL); 
+    handle = curl_easy_init();
+    handle2 = curl_easy_duphandle(handle);
+
+    std::thread t1(thread_function);
+
+    while(!check);
+
+    curl_easy_setopt(handle, CURLOPT_URL, "https://ifconfig.me/");
+    std::cout<<"IP Address From Thread 1\n";
+    curl_easy_perform(handle);
+    std::cout<<std::endl;
+
+    t1.join();
+
+    /* std::cout<<"Interface List Before Unshare\n";
     handle.get_network_interfaces();
 
     if(net_interface_handler::net_ns_unshare())
@@ -12,7 +52,7 @@ int main() {
     handle.add_network_interface(0);
 
     std::cout<<"Interface List After Adding To This Namespace\n";
-    handle.get_network_interfaces();
+    handle.get_network_interfaces(); */
 
     return 0;
 }
